15a082c790271eac1be7ae5ee414ecc2e6aad3b7
Yusuf Shamsudeen <yusufshamsudeen77@gmail.com>
2023-11-06 16:21:10 +0100

    change of image directory

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import logo from '../Images/homepage/Toshconsult Img 1.png'
-
+import logo from './Images/homepage/Toshconsult Img 1.png'
+import blurr from './Images/homepage/blurr.png'
 function NavBar() {
   return (
+    <div>
+    <div>
+      <img className='absolute' src={blurr} alt="" />
+    </div>
     <div className='navbar flex items-center justify-around py-9 '>
       <div>
         <img src={logo} alt="toshlogo" />
       </div>
       <div>
-        <ul className='flex justify-between gap-3'>
-            <li >Home </li>
-            <li>About</li>
-            <li>Courses</li>
-            <li>Services</li>
-            <li>Career</li>
-            <li>Contact</li>
+        <ul className='flex justify-between gap-6 font-raleway font-semibold '>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer '>Home </li>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer'>About</li>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer'>Courses</li>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer'>Services</li>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer'>Career</li>
+            <li className='hover:text-[#FB9B02] transition duration-300 cursor-pointer'>Contact</li>
         </ul>
       </div>
       <div>
-        <button className='bg-[#FB9B02] px-5 py-2 rounded-[14px] text-[white]'>Apply Now</button>
+        <button className='bg-[#FB9B02] px-5 py-2 rounded-[14px] text-[white] border-transparent  hover:bg-[white]  hover:text-[#FB9B02] hover:border hover:border-solid hover:border-[#FB9B02] transition duration-200'>Apply Now</button>
       </div>
+    </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+      
+    },
   },
   plugins: [],
-}
-
+};
